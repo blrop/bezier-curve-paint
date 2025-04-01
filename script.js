4d9a3ad0640f8f0c4b8316c9e1307a5368bd8dab
Igor Siluianov <olvin.hh@gmail.com>
2025-04-01 03:04:37 +0300

    add bezier curve painting

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
+const colorGreen = 'rgb(34, 133, 49)';
+const colorRed = 'rgb(133, 51, 34)';
+
+const point1 = { x: 150, y: 555 };
+const point2 = { x: 405, y: 60 };
+const point3 = { x: 1100, y: 160 };
+
 const $canvas = document.getElementById('canvas');
-const ctx = $canvas.getContext("2d");
+const ctx = $canvas.getContext('2d');
 
-const x1 = 25;
-const y1 = 200;
-const x2 = 500;
-const y2 = 25;
+// $canvas.addEventListener('click', (e) => {
+//     console.log(e.offsetX, e.offsetY);
+// });
 
-ctx.strokeStyle = 'rgb(255, 151, 160)';
-ctx.beginPath();
-ctx.moveTo(x1, y1);
-ctx.lineTo(x2, y2);
-ctx.stroke();
+const getPointByPercent = (point1, point2, percent) => {
+    const x = (point2.x * percent + point1.x * (100 - percent)) / 100;
+    const y = (point2.y * percent + point1.y * (100 - percent)) / 100;
 
-const percent = 75;
-const x = (x2 * percent + x1 * (100 - percent)) / 100;
-const y = (y2 * percent + y1 * (100 - percent)) / 100;
+    return { x, y };
+}
 
-ctx.beginPath();
-ctx.arc(x, y, 10, 0, Math.PI * 2);
-ctx.stroke();
+const setColor = (color) => {
+    ctx.strokeStyle = color;
+};
+
+const drawLine = (point1, point2) => {
+    ctx.beginPath();
+    ctx.moveTo(point1.x, point1.y);
+    ctx.lineTo(point2.x, point2.y);
+    ctx.stroke();
+};
+
+const setLineWidth = (width) => {
+    ctx.lineWidth = width;
+};
+
+drawCircle = (point, radius) => {
+    ctx.beginPath();
+    ctx.arc(point.x, point.y, radius, 0, Math.PI * 2);
+    ctx.stroke();
+};
+
+const p1 = { x: 25, y: 200 };
+const p2 = { x: 500, y: 25 };
+
+setLineWidth(3);
+
+setColor(colorGreen);
+drawLine(point1, point2);
+drawLine(point2, point3);
+
+setColor(colorRed);
+
+let oldPoint = point1;
+for (let i = 0; i <= 100; i++) {
+    const point12 = getPointByPercent(point1, point2, i);
+    const point23 = getPointByPercent(point2, point3, i);
+    const currentPoint = getPointByPercent(point12, point23, i);
+
+    drawLine(oldPoint, currentPoint);
+    oldPoint = currentPoint;
+}
