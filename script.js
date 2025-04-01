93a13d043770afde02fe65b14c4e2c0ba3a4620d
Igor Siluianov <olvin.hh@gmail.com>
2025-04-01 03:13:15 +0300

    add points selection by mouse

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 const colorGreen = 'rgb(34, 133, 49)';
 const colorRed = 'rgb(133, 51, 34)';
 
-const point1 = { x: 150, y: 555 };
-const point2 = { x: 405, y: 60 };
-const point3 = { x: 1100, y: 160 };
-
 const $canvas = document.getElementById('canvas');
 const ctx = $canvas.getContext('2d');
-
-// $canvas.addEventListener('click', (e) => {
-//     console.log(e.offsetX, e.offsetY);
-// });
 
 const getPointByPercent = (point1, point2, percent) => {
     const x = (point2.x * percent + point1.x * (100 - percent)) / 100;
@@ -34,29 +26,53 @@ const setLineWidth = (width) => {
     ctx.lineWidth = width;
 };
 
+const clear = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
 drawCircle = (point, radius) => {
     ctx.beginPath();
     ctx.arc(point.x, point.y, radius, 0, Math.PI * 2);
     ctx.stroke();
 };
 
-const p1 = { x: 25, y: 200 };
-const p2 = { x: 500, y: 25 };
+const drawBezierCurve = (point1, point2, point3) => {
+    setColor(colorGreen);
+    setLineWidth(1);
+    drawLine(point1, point2);
+    drawLine(point2, point3);
 
-setLineWidth(3);
+    setColor(colorRed);
+    setLineWidth(3);
 
-setColor(colorGreen);
-drawLine(point1, point2);
-drawLine(point2, point3);
+    let oldPoint = point1;
+    for (let i = 0; i <= 100; i++) {
+        const point12 = getPointByPercent(point1, point2, i);
+        const point23 = getPointByPercent(point2, point3, i);
+        const currentPoint = getPointByPercent(point12, point23, i);
 
-setColor(colorRed);
+        drawLine(oldPoint, currentPoint);
+        oldPoint = currentPoint;
+    }
+};
 
-let oldPoint = point1;
-for (let i = 0; i <= 100; i++) {
-    const point12 = getPointByPercent(point1, point2, i);
-    const point23 = getPointByPercent(point2, point3, i);
-    const currentPoint = getPointByPercent(point12, point23, i);
+let selectedPoints = [];
 
-    drawLine(oldPoint, currentPoint);
-    oldPoint = currentPoint;
-}
+$canvas.addEventListener('click', (e) => {
+    if (selectedPoints.length < 3) {
+        const point = { x: e.offsetX, y: e.offsetY };
+        selectedPoints.push(point);
+
+        setColor(colorRed);
+        setLineWidth(3);
+        drawCircle(point, 10);
+
+        if (selectedPoints.length === 3) {
+            clear();
+            drawBezierCurve(...selectedPoints);
+        }
+    } else if (selectedPoints.length === 3) {
+        clear();
+        selectedPoints = [];
+    }
+});
